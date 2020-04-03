32dac051a0830c4cda8c970a0b8b07bd4b0f285a
easonccc <842060876@qq.com>
2020-04-03 13:12:50 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
+<script>
+export default {};
+</script>
 <style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,18 +14,17 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body,
+html {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/map.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="box">
+    <div id="container"></div>
+  </div>
+</template>
+
+<script>
+import AMap from "AMap"; // 引入高德地图
+export default {
+  name: "mymap",
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      let map = new AMap.Map("container", {
+        center: [116.397428, 39.90923],
+        resizeEnable: true,
+        zoom: 10
+      });
+    }
+  }
+};
+</script>
+<style lang="less">
+.box {
+  flex: 1;
+  display: flex !important;
+  flex-direction: row;
+  flex-wrap: wrap;
+  #container {
+    flex: 1;
+  }
+}
+</style>
